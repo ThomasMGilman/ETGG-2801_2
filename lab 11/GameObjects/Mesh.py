6d2543f892e798b45e7aeaafcb8e8e8f69b22a32
ThomasMGilman <Thomas.Gilman@ymail.com>
2019-11-20 14:02:27 -0500

    commented Mesh code and added reading in of normals

--- lab 11/GameObjects/Mesh.py
+++ lab 11/GameObjects/Mesh.py
@@ -9,86 +9,102 @@ class Mesh:
         self.fname = os.path.join("assets",fname)
         self.materialDict
         self.curMat
-        V = []
-        T = []
-        I = []
+        VertexPoints = []           # Vertex Vec3 Points for Mesh
+        Normals = []
+        TexturePoints = []          # Texture Vec2 Points for Mesh
+        IndexPoints = []            # Index Points in vertex points and texture points
 
         with open(fname) as fp:
             for line in fp:
                 line = line.strip()
-                if  line.startswith("#"):           #line is a Comment
+                if  line.startswith("#"):           # line is a Comment ignore it
                     continue
 
-                elif line.startswith("mtlib"):      #Get Meshes material file name and get its textures
+                elif line.startswith("mtlib"):      # Get Meshes material file name and get its textures
                     matfname = line[7:]
                     self.materialDict = self.parseMtl(matfname)
 
-                elif line.startswith("v "):         #Get Meshes vertex points
+                elif line.startswith("v "):         # Get Meshes vertex points
                     vertices = line.split()
-                    V.append(vec3(float(vertices[1]), float(vertices[2]), float(vertices[3])))
+                    VertexPoints.append(vec3(float(vertices[1]), float(vertices[2]), float(vertices[3])))
 
-                elif line.startswith("vt"):
+                elif line.startswith("vt"):         # Get Meshes Texture points
                     texturePoints = line.split()
-                    T.append(vec2(float(texturePoints[1]), float(texturePoints[2])))
+                    TexturePoints.append(vec2(float(texturePoints[1]), float(texturePoints[2])))
 
-                elif line.startswith("usemtl"):
+                elif line.startswith("vn"):         # Get Meshes Normal points
+                    normalPoints = line.split()
+                    Normals.append(vec3(float(normalPoints[1]), float(normalPoints[2]), float(normalPoints[3])))
+
+                elif line.startswith("usemtl"):     # Set CurrentMaterial
                     curMat = line.split()[1]
-                    if curMat not in I:
-                        I[curMat] = []
+                    if curMat not in IndexPoints:
+                        IndexPoints[curMat] = []
 
-                elif line.startswith("f "):
-                    L = line.split()
-                    if len(L) != 4:
+                elif line.startswith("f "):         # Set CurrentMaterials Vertex Points, Texture Points, and Normals
+                    fLine = line.split()
+                    if len(fLine) != 4:
                         raise Exception("Not Triangles")
 
-                    for VSpec in L[1:]:
-                        L2 = VSpec.split('/')
-                        if len(L2) != 3:
-                            raise Exception("Expected vi / ti / ni, instead got: ", L2)
+                    for VSpec in fLine[1:]:
+                        pointPack = VSpec.split('/')
+                        if len(pointPack) != 3:
+                            raise Exception("Expected vi / ti / ni, instead got: ", pointPack)
 
-                        if L2[1] == "":
-                            raise Exception("Expected vertex index, instead got None!!! L2 contains: ", L2)
+                        if pointPack[1] == "":
+                            raise Exception("Expected vertex index, instead got None!!! pointPack contains: ", pointPack)
 
-                        vi = int(L2[0]) - 1
-                        ti = int(L2[1]) - 1
-                        I[curMat].append((vi, ti))
+                        vi = int(pointPack[0]) - 1              # Set Vertice Index
+                        ti = int(pointPack[1]) - 1              # Set Texture Index
+                        ni = int(pointPack[2]) - 1              # Set Normal Index
+                        IndexPoints[curMat].append((vi, ti, ni))
 
-        tmp = array.array("I", [0])
+        tmp = array.array("IndexPoints", [0])       # Set vao
         glGenVertexArrays(1, tmp)
         self.vao = tmp[0]
 
-        vmap = {}               #key = (vi,ti), Val = index
+        vmap = {}               # key = (vi,ti,ni), Val = index
         p = []
         t = []
+        n = []
         i = []
         numV = 0
-        self.matDict = {}       #key = matName, Val = (startIndex, size)
+        self.matDict = {}       # key = matName, Val = (startIndex, size)
 
-        for mname in I:
-            for vi,ti in I[mname]:
-                key = (vi,ti)
+        for matName in IndexPoints:                 # Foreach key
+            for vi, ti, ni in IndexPoints[matName]:     # Foreach value pair
+                key = (vi, ti, ni)
 
-                if key not in vmap:
+                if key not in vmap:                         # Append key to dictionary if not already there
                     vmap[key] = numV
                     numV += 1
-                    p.append(V[vi].x)
-                    p.append(V[vi].y)
-                    p.append(V[vi].z)
 
-                    t.append(T[ti].x)
-                    t.append(T[ti].y)
+                    # Set Vertex Values, 3 floats per point
+                    p.append(VertexPoints[vi].x)
+                    p.append(VertexPoints[vi].y)
+                    p.append(VertexPoints[vi].z)
 
-                i.append(vmap[key])
+                    # Set Texture Values, 2 floats per point
+                    t.append(TexturePoints[ti].x)
+                    t.append(TexturePoints[ti].y)
 
-            self.matDict[mname] = (len(i) * 4, len(I[mname]))
+                    # Set Normal values, 3 floats per point
+                    n.append(Normals[ni].x)
+                    n.append(Normals[ni].y)
+                    n.append(Normals[ni].z)
 
-        pointBuf = Buffer(array.array("f", p))
-        texBuf = Buffer(array.array("f", t))
-        indexBuf = Buffer(array.array("I", i))
-        glBindVertexArray(self.vao)
+                i.append(vmap[key])                        # Append Index into dictionary for point value set
 
-    """Add the new Materail to the dictionary"""
+            # Set Start starting point into point set, and number of indices for point set for material
+            self.matDict[matName] = (len(i) * 4, len(IndexPoints[matName]))
+
+        pointBuf    = Buffer(array.array("f", p))           # Create Point Buffer
+        texBuf      = Buffer(array.array("f", t))           # Create Texture Buffer
+        indexBuf    = Buffer(array.array("IndexPoints", i)) # Create Index buffer for index points
+        glBindVertexArray(self.vao)                         # Bind vao
+
     def parseMtl(self, fname):
+        """Add the new Materail to the dictionary"""
         MeshColors = {}
         with open(os.path.join("assets", fname)) as fp:
             for line in fp:
@@ -103,8 +119,8 @@ class Mesh:
     def draw(self):
         glBindVertexArray(self.vao)
         for mname in self.matDict:
-            self.materialDict[mname].bind(0)
-            start, count = self.matDict[mname]
+            self.materialDict[mname].bind(0)        # Bind Texture
+            start, count = self.matDict[mname]      # Set start index, and len of indicies
 
             glDrawElements(
                 GL_TRIANGLES,
